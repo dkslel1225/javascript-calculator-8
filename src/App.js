350b54cdb25557c7f0124f7faf9cbcbf27b1429f
jyn <spdlqjdkslel1226@naver.com>
2025-10-15 18:26:40 +0900

    feat: calculator 입출력 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { calculator } from "./calculator.js";
+
 class App {
-  async run() {}
+  async run() {
+    calculator();
+  }
 }
 
 export default App;
---
+++ src/calculator.js
@@ -0,0 +1,6 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export const calculator = async () => {
+  const result = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+  Console.print(`결과 : ${result}`);
+};

ffb928eb0a55d6417313efb36dd06c2ffe6619a1
jyn <spdlqjdkslel1226@naver.com>
2025-10-17 23:28:51 +0900

    feat: getOperands 구현 (피연산자 체크 및 배열로 반환)

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
 import { checkSeparator } from "./checkSeparator.js";
+import { getOperands } from "./getOperands.js";
 
 export const calculator = async () => {
   const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
   const str = input.replace(/\s/g, ""); // 공백 제거
   const separator = checkSeparator(str);
+  const operands = getOperands(str, separator);
   Console.print(`결과 : ${result}`);
 };
---
+++ src/getOperands.js
@@ -0,0 +1,13 @@
+export const getOperands = (str, separator) => {
+  const operands =
+    separator === null ? str.split(/[,:]/) : str.slice(5).split(separator);
+  isValidExpression(operands);
+  return operands;
+};
+
+const isValidExpression = (operands) => {
+  operands.forEach((operand) => {
+    if (isNaN(operand)) throw new Error("숫자가 아닌 값이 있습니다.");
+    if (Number(operand) < 0) throw new Error("음수는 계산할 수 없습니다.");
+  });
+};
